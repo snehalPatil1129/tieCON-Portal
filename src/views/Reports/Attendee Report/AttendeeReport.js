5aef1b7cc147858c312933987cfac5359e95fc96
Snehal Kale <snehal.kale@eternussolutions.com>
2018-06-05 14:34:04 +0530

    changes due to speaker separation from attendee table(attendeeReport)

--- src/views/Reports/Attendee Report/AttendeeReport.js
+++ src/views/Reports/Attendee Report/AttendeeReport.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {Bar, Doughnut, Line, Pie, Polar, Radar} from 'react-chartjs-2';
-import {CardColumns, Card, CardHeader, CardBody, Row, Col, FormGroup} from 'reactstrap';
+import React, { Component } from 'react';
+import { Bar, Doughnut, Line, Pie, Polar, Radar } from 'react-chartjs-2';
+import { CardColumns, Card, CardHeader, CardBody, Row, Col, FormGroup } from 'reactstrap';
 import _ from 'lodash';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
@@ -8,109 +8,124 @@ import { DBUtil } from '../../../services';
 import Highcharts from 'highcharts';
 
 class AttendeeReport extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
-            userProfiles :[],
-            attendeeList : [],
-            attendeeCounter : []
+            userProfiles: [],
+            attendeeList: [],
+            attendeeCounter: []
         }
     }
 
-    componentWillMount () {
+    componentWillMount() {
         this.getUserProfiles();
         this.getAttendees();
     }
 
-    getUserProfiles () {
+    getUserProfiles() {
         let thisRef = this;
         DBUtil.getDocRef("UserProfiles")
-        .get()
-        .then((snapshot) => {
-            let updatedProfiles = [...thisRef.state.userProfiles];
-            snapshot.forEach(function (doc) {
-                updatedProfiles.push( doc.data().profileName);
+            .get()
+            .then((snapshot) => {
+                let updatedProfiles = [...thisRef.state.userProfiles];
+                snapshot.forEach(function (doc) {
+                    updatedProfiles.push(doc.data().profileName);
+                });
+                thisRef.setState({
+                    userProfiles: updatedProfiles
+                });
+            })
+            .catch((err) => {
+                console.log("Error", err);
             });
-            thisRef.setState({
-                userProfiles : updatedProfiles
-           });
-        })
-        .catch((err) =>{
-            console.log("Error", err);
-        });
     }
 
-    getAttendees () {
+    getAttendees() {
         let thisRef = this;
+        let updatedAttendeeList = [...thisRef.state.attendeeList];
         DBUtil.getDocRef("Attendee")
-        .get()
-        .then((snapshot) => {
-            let updatedAttendeeList = [...thisRef.state.attendeeList];
-            snapshot.forEach(doc => {
-                updatedAttendeeList.push({profiles : doc.data().profileServices[0] , name : doc.data().fullName});
+            .get()
+            .then((snapshot) => {
+
+                snapshot.forEach(doc => {
+                    if (doc.data().profileServices[0] != "Speaker")
+                        updatedAttendeeList.push({ profiles: doc.data().profileServices[0], name: doc.data().fullName });
+                });
+                DBUtil.getDocRef("Speakers")
+                    .get()
+                    .then((snapshot) => {
+                        snapshot.forEach(doc => {
+                            updatedAttendeeList.push({ profiles: "Speaker", name: doc.data().fullName });
+                        });
+                        thisRef.setState({
+                            attendeeList: updatedAttendeeList
+                        });
+                        thisRef.getCounts();
+                    })
+                // thisRef.setState({
+                //     attendeeList : updatedAttendeeList
+                // });
+                // thisRef.getCounts();
+            })
+            .catch((err) => {
+                console.log("Error", err);
             });
-            thisRef.setState({
-                attendeeList : updatedAttendeeList
-            });
-            thisRef.getCounts();
-        })
-        .catch((err) =>{
-            console.log("Error", err);
-        });
     }
 
-    getCounts () {
+    getCounts() {
         let profileRoles = this.state.userProfiles;
-        let attendeeProfileDetails =this.state.attendeeList;
+        let attendeeProfileDetails = this.state.attendeeList;
+
         let attendeeCounter = [];
 
-        profileRoles.forEach( (role , index) => {
-            if(index == 0) {
-                attendeeCounter.push({ name : role, y: _.filter(attendeeProfileDetails, { profiles: role }).length , sliced : true , selected :true })                
+        profileRoles.forEach((role, index) => {
+            if (index == 0) {
+                attendeeCounter.push({ name: role, y: _.filter(attendeeProfileDetails, { profiles: role }).length, sliced: true, selected: true })
             }
-            else{
-                attendeeCounter.push({ name : role, y: _.filter(attendeeProfileDetails, { profiles: role }).length })                
+            else {
+                attendeeCounter.push({ name: role, y: _.filter(attendeeProfileDetails, { profiles: role }).length })
             }
         })
         this.setState({
-            attendeeCounter : attendeeCounter
+            attendeeCounter: attendeeCounter
         });
-        this.createPieChart ();
+   
+        this.createPieChart();
     }
 
     createPieChart() {
-            Highcharts.chart('profileReport', {
-                chart: {
-                    plotBackgroundColor: null,
-                    plotBorderWidth: null,
-                    plotShadow: false,
-                    type: 'pie'
-                },
-                title: {
-                    text: 'Profile Report'
-                },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.y}</b>'
-                },
-                plotOptions: {
-                    pie: {
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        dataLabels: {
-                            enabled: true,
-                            format: '<b>{point.name}</b>: {point.y} ',
-                            style: {
-                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                            }
+        Highcharts.chart('profileReport', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Profile Report'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.y}</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.y} ',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
                         }
                     }
-                },
-                series: [{
-                    name: 'Profile Report',
-                    colorByPoint: true,
-                    data: this.state.attendeeCounter
-                }]
-            });
+                }
+            },
+            series: [{
+                name: 'Profile Report',
+                colorByPoint: true,
+                data: this.state.attendeeCounter
+            }]
+        });
     }
     render() {
         return (
@@ -124,7 +139,7 @@ class AttendeeReport extends Component {
                                         Profile Report
                                     </CardHeader>
                                     <CardBody>
-                                            <div id="profileReport" style={{minwidth: "310px", height: "400px", maxwidth: "600px", margin: "0 auto"}} ></div>
+                                        <div id="profileReport" style={{ minwidth: "310px", height: "400px", maxwidth: "600px", margin: "0 auto" }} ></div>
                                     </CardBody>
                                 </Card>
                             </Col>
